fd358964a915054932b23a40b93f972024998f3d
Abhinav Kumar <kumabhi139@gmail.com>
2026-04-27 15:42:27 +0530

    Initial setup (project structure)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import Navbar from "./components/Navbar"
+import About from "./section/About"
+import Contact from "./section/Contact"
+import Experience from "./section/Experience"
+import Footer from "./section/Footer"
+import Home from "./section/Home"
+import Projects from "./section/Projects"
+import Skills from "./section/Skills"
+import Testimonial from "./section/Testimonial"
 
-
-function App() {
-  
-  return (
-    <>
-      
-    </>
+export default function App(){
+  return(
+    <div>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Experience/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
+    </div>
   )
 }
-
-export default App
---
+++ src/section/Testimonial.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Testimonial
+ = () => {
+  return (
+    <div className='w-full h-screen'>
+        Testimonial
+    </div>
+  )
+}
+
+export default Testimonial
