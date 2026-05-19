3a2d851a332d01e589dd52528596b55b4901f821
Abhinav Kumar <kumabhi139@gmail.com>
2026-05-20 00:41:48 +0530

    Fixed Vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production'
-    ? '/Abhinav_Dev_Hub_Portfolio/'
-    : '/',
+   base: '/Abhinav_Dev_Hub_Portfolio/',
 })
